526b93265fd7cffb6036af5b6188139c6c8863c4
Fedi Beltaief <fediblt3@gmail.com>
2023-11-11 11:49:46 +0100

    Initial commit

---
+++ Event/Event/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Event
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+        public void Display(Object sender, EventArgs e)
+        {
+            lblMessage.Text = "Welecome Mr. " + txtName.Text;   
+        }
+
+
+    }
+}
